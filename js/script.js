24261c088b65dcd8f5a37fe333d607c1c2e46939
Strategy102 <strategy954@gmail.com>
2022-08-29 12:53:10 -0700

    Personal Portfolio

---
+++ js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    });
+
+    $(window).on('scroll load',function(){
+
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+
+        if($(window).scrollTop() > 0){
+            $('.top').show();
+        }else{
+            $('.top').hide(); 
+        }
+    });
+    
+    // smooth srolling
+    $('a[href*="#"]').on('click',function(e){
+        e.preventDefault();
+
+        $('html, body').animate({
+
+            scrollTop : $($(this).attr('href')).offset().top,
+
+        },
+            250,
+            'linear'
+        );
+    });
+
+});
+
+let themeToggler = document.querySelector('#theme-toggler');
+
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-moon');
+    if(themeToggler.classList.contains('fa-moon')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+}    
---
+++ js/style.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    Strings:["", "Abubakri Usamo", "Oladimeji"],
+    typeSpeed:100,
+    BakSpeed:60,
+    loop:true
+})
